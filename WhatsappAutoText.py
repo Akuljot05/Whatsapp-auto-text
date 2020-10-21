0cc06334b3f2f324b763e087cebd0f1b2e88b77d
csgulati09 <chandeepsinghgulati@gmail.com>
2020-10-21 22:15:00 +0530

    driver added and target box fixed

--- WhatsappAutoText.py
+++ WhatsappAutoText.py
@@ -12,6 +12,9 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
+# Replace below path with the absolute path
+# to chromedriver in your computer
+driver = webdriver.Chrome('/<PATH TO DRIVER>/chromedriver')
 
 driver.get("https://web.whatsapp.com/")
 wait = WebDriverWait(driver, 600)
@@ -29,7 +32,7 @@ test = line.split('\n\n')
 
 string = "Python Text"
 
-
+x_arg = '//span[contains(@title,' + target + ')]'
 group_title = wait.until(EC.presence_of_element_located((By.XPATH, x_arg)))
 group_title.click()
 inp_xpath = '/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div[2]/div/div[2]'
